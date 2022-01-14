4e79309288bbaa7c62eef76186116ff39a7440e0
liubovsergheeva <84126939+liubovsergheeva@users.noreply.github.com>
2022-01-14 15:09:21 +0300

    Add files via upload

---
+++ Python-HW_1-Console.py
@@ -0,0 +1,22 @@
+
+hello <class 'str'>
+100 <class 'int'>
+165.5 <class 'float'>
+b'this is massive of bytes' <class 'bytes'>
+b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' <class 'bytes'>
+[True, 777, 3.11, 'text', 70.1] <class 'list'>
+(True, 777, 3.12, 'eto text', 77.2) <class 'tuple'>
+{'6', '5', '3', '1', '2', '4'} <class 'set'>
+{'t', 's', 'e'} <class 'set'>
+{'hi', 'bye'} <class 'set'>
+frozenset({'6', '5', '3', '1', '2'}) <class 'frozenset'>
+frozenset({'6', 'x', 's', 'a', '1', '2', 'e', 't', 'k', '4'}) <class 'frozenset'>
+frozenset({'q', 'y', 'r', 'w', 'e', 't'}) <class 'frozenset'>
+<class 'dict'> <class 'type'>
+123555 <class 'str'>
+hello , 100 <class 'str'> <class 'int'>
+hello 100 <class 'str'> <class 'int'>
+hello100 <class 'str'> <class 'int'>
+123  100 <class 'str'> <class 'int'>
+
+Process finished with exit code 0
