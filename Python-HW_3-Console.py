1ebefd42b7160973b07eb82ba480093df93d25d6
liubovsergheeva <84126939+liubovsergheeva@users.noreply.github.com>
2022-02-02 20:58:03 +0300

    Add files via upload

---
+++ Python-HW_3-Console.py
@@ -0,0 +1,34 @@
+
+Переменная mult_int больше 18
+Переменная div_int меньше 18
+Переменная item_1 больше или равна 18
+Переменная item_2 = True
+Переменная item_3 =  False
+Переменная item_5 =  1
+Переменная item_4 =  0
+введите валюту [eur /uah /chf /rub /byn]: eur
+введите количество 100
+100 eur = 116.27906976744187 usd
+currency_usd = usd
+введите валюту [eur /uah /chf /rub /byn]: uah
+введите количество 100
+100 uah = 3.8124285169653067 usd
+currency_usd = usd
+введите валюту [eur /uah /chf /rub /byn]: chf
+введите количество 100
+100 chf = 109.89010989010988 usd
+currency_usd = usd
+введите валюту [eur /uah /chf /rub /byn]: rub
+введите количество 100
+100 rub = 1.391207568169171 usd
+currency_usd = usd
+введите валюту [eur /uah /chf /rub /byn]: byn
+введите количество 100
+100 rub = 40.65040650406504 usd
+currency_usd = usd
+введите валюту [eur /uah /chf /rub /byn]: asd
+введите количество 0
+Unknown currency
+currency_usd = usd
+
+Process finished with exit code 0
